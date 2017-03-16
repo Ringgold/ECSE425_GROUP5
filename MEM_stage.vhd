ed54416cfed1bb04aff3df518f25d5a79f1bd759
Ringgold <ringgoldlin@yahoo.com>
2017-03-16 09:30:04 +0800

    Updaed MEM and WB

--- MEM_stage.vhd
+++ MEM_stage.vhd
@@ -10,48 +10,73 @@ entity MEM_stage is
 	);
 	port(
 		clock : in std_logic;
-		memRead : in std_logic;
-		memWrite : in std_logic;
-		address: IN INTEGER RANGE 0 TO ram_size-1;
-		writeData: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		result: out INTEGER RANGE 0 TO ram_size-1;
-		waitrequest: OUT STD_LOGIC
+		--rdy: in std_logic; -- indicates if the stall is over
+		opcode : in std_logic_vector(5 downto 0); --operation code
+		register_data: in std_logic_vector(31 downto 0); --RD2
+    	alu_result: in std_logic_vector(31 downto 0); -- redult from ALU
+    	destination_addr: in std_logic_vector (4 downto 0);
+
+    	memory_data: out std_logic_vector(31 downto 0); --passed from memory to WB
+    	alu_result_go: out std_logic_vector(31 downto 0); -- redult from ALU to be forwarded to WB
+    	writeback_addr: out std_logic_vector(4 downto 0);
+
 	);
 end MEM_stage;
 
 architecture beh of MEM_stage is
 	signal clk : std_logic;
-	signal wd : std_logic_vector(7 downto 0);
-	signal add : IN INTEGER RANGE 0 TO ram_size-1;
+	signal wd : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+	signal alu : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 	signal mw : std_logic;
 	signal mr : std_logic;
-	signal rd : std_logic_vector(7 downto 0);
-	signal wr : std_logic;
+	signal rd : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
   
   	component data_memory
     	port(
 	      	clock: IN STD_LOGIC;
-			writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-			address: IN INTEGER RANGE 0 TO ram_size-1;
+			writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+			address: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 			memwrite: IN STD_LOGIC;
 			memread: IN STD_LOGIC;
-			readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-			waitrequest: OUT STD_LOGIC
+			readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
     	);
   	end component;
   
 begin
-MEM1: data_memory port map(clk, wd, add, mw, mr, rd, wr);
+MEM1: data_memory port map(
+	clock: => clk, 
+	writedata: => wd, 
+	address: => alu, 
+	memwrite: => mw, 
+	memread: => mr, 
+	readdata: => rd
+	);
 
-	clk <= clock;
-	wd <= writeData;
-	add <= address;
-	mw <= memWrite;
-	mr <= memRead;		
-	rd <= readdata;
-	wr <= waitrequest;
-	result <= address;
+For_DM : process
+begin
+
+    if rising_edge(clock) then
+
+      	clk <= clock;
+      	alu <= "00000000000000000" & alu_result(14 downto 0);
+
+      	--Set read and write signals
+      	mr <= '1' when opcode = "100011";
+      	mw <= '1' when opcode = "101011";
+
+    end if;
+
+end process;
+
+
+Stage_process : process (clock)
+begin
+  	if rising_edge(clock) then
+	    alu_result <= alu_result_go;
+	    writeback_addr <= destination_addr;
+	    memory_data <= rd;
+    end if;
+end process;
 	
 end beh;
 
--- data_memory.vhd
+++ data_memory.vhd
@@ -11,60 +11,43 @@ ENTITY data_memory IS
 	);
 	PORT (
 		clock: IN STD_LOGIC;
-		writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		address: IN INTEGER RANGE 0 TO ram_size-1;
+		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		--address: IN INTEGER RANGE 0 TO ram_size-1;
+		address: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		memwrite: IN STD_LOGIC;
 		memread: IN STD_LOGIC;
-		readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		waitrequest: OUT STD_LOGIC
+		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
 	);
 END data_memory;
 
 ARCHITECTURE rtl OF data_memory IS
-	TYPE MEM IS ARRAY(ram_size-1 downto 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+	TYPE MEM IS ARRAY(ram_size-1 downto 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL ram_block: MEM;
 	SIGNAL read_address_reg: INTEGER RANGE 0 to ram_size-1;
+	SIGNAL address_int: INTEGER RANGE 0 to ram_size-1;
 	SIGNAL write_waitreq_reg: STD_LOGIC := '1';
 	SIGNAL read_waitreq_reg: STD_LOGIC := '1';
 BEGIN
-	--initialize the data_memory to 0 at start
+	
 	mem_process: PROCESS (clock)
 	BEGIN
-		--This is a cheap trick to initialize the SRAM in simulation
+		--initialize the data_memory value all to 0 at start
 		IF(now < 1 ps)THEN
 			For i in 0 to ram_size-1 LOOP
-				ram_block(i) <= std_logic_vector(to_unsigned(0,8));
+				ram_block(i) <= std_logic_vector(to_unsigned(0,32));
 			END LOOP;
 		end if;
 
 		--This is the actual synthesizable SRAM block
 		IF (clock'event AND clock = '1') THEN
+			address_int <= to_integer(unsigned(address));
 			IF (memwrite = '1') THEN
-				ram_block(address) <= writedata;
+				ram_block(address_int) <= writedata;
 			END IF;
-		read_address_reg <= address;
+		read_address_reg <= address_int;
 		END IF;
 	END PROCESS;
 	readdata <= ram_block(read_address_reg);
-
-
-	--The waitrequest signal is used to vary response time in simulation
-	--Read and write should never happen at the same time.
-	waitreq_w_proc: PROCESS (memwrite)
-	BEGIN
-		IF(memwrite'event AND memwrite = '1')THEN
-			write_waitreq_reg <= '0' after mem_delay, '1' after mem_delay + clock_period;
-
-		END IF;
-	END PROCESS;
-
-	waitreq_r_proc: PROCESS (memread)
-	BEGIN
-		IF(memread'event AND memread = '1')THEN
-			read_waitreq_reg <= '0' after mem_delay, '1' after mem_delay + clock_period;
-		END IF;
-	END PROCESS;
-	waitrequest <= write_waitreq_reg and read_waitreq_reg;
 
 
 END rtl;
